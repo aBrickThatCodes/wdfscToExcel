5a525e3524ec5414577904c56b24eb1a200c418c
aBrickThatCodes <39009094+aBrickThatCodes@users.noreply.github.com>
2020-10-16 19:44:44 +0200

    Add files via upload

--- dataToExcel.py
+++ dataToExcel.py
@@ -1,6 +1,6 @@
 #! python 3
 # dataToExcel.py - Place all data in the dir in an excel file.
-# usage: dataToExcel.py <path> - pakuje wszystkie pliki tekstowe w <path> do .xlsx w <path>
+# usage: dataToExcel.py <ind> <path> - pakuje plik z danymi dla zespołu wygenerowanego z <ind> w <path> do .xlsx w <path>
 
 import openpyxl
 import os
@@ -8,10 +8,13 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description='Place all data in the dir in an excel file.')
+parser.add_argument('ind',type=int,help='Your index')
 parser.add_argument('path', type=str, help='Path to the directory with the files')
 
 
-path = vars(parser.parse_args())['path']
+args=vars(parser.parse_args())
+path = args['path']
+ind = args['ind']
 name = ''
 try:
     name = path.split(os.sep)[-1]
@@ -28,18 +31,17 @@ workbook = openpyxl.Workbook()
 for filename in filenames:
     if not filename.endswith('.txt'):
         continue
-    if not filename[:-4] in workbook.sheetnames:
-        workbook.create_sheet(title=filename[:-4])
-    sheet = workbook[filename[:-4]]
-    with open(filename, 'rt') as file:
-        row = 1
-        for line in file:
-            column = 1
-            for item in line.split():
-                sheet.cell(row=row, column=column).value = item
-                column += 1
-            row += 1
+    if filename[-5]==str(ind%5+1): 
+        sheet = workbook.active
+        sheet.name = filename[:-4]
+        with open(filename, 'rt') as file:
+            row = 1
+            for line in file:
+                column = 1
+                for item in line.split():
+                    sheet.cell(row=row, column=column).value = item
+                    column += 1
+                row += 1
 
-del workbook['Sheet']
 workbook.save(name+'.xlsx')
 input("Finished.")
